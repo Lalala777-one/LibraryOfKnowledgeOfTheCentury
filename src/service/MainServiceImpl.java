8d4db7b3837e8e00bef0a0fa6e8f89b373fd56ba
Alla-Nazarenko <umnyj.start@gmail.com>
2024-10-30 13:06:18 +0100

    Service methods

--- src/service/MainServiceImpl.java
+++ src/service/MainServiceImpl.java
@@ -4,9 +4,10 @@ import model.Book;
 import model.User;
 import repository.BookRepository;
 import repository.UserRepository;
+import utils.MyArrayList;
 import utils.MyList;
 
-public class MainServiceImpl  implements  MainService{
+public class MainServiceImpl implements MainService {
 
     private final BookRepository repositoryBook;
     private final UserRepository repositoryUser;
@@ -60,39 +61,82 @@ public class MainServiceImpl  implements  MainService{
 
     //-----------------------------------------Alla Nazarenko------------------------------
 
-    @Override
+    //-----------------------------------------Methods of Books------------------------------
+
+        @Override
     public boolean takeBook(int id) {
          /*
-        получить книгу из репозитория
+        получить книгу из репозитория и присвоить её в переменную
         проверить, что она найдена и свободна,
         если нет - закончить работу метода,
-        если да  - пометить машину как занятую,
-                   добавить машину в список книг текущего пользователя
+        если да  - пометить книгу как занятую,
+                   добавить книгу в список книг текущего пользователя
          */
         Book book = repositoryBook.getBookById(id);
         if (book == null || book.isBusy()) {
+            System.out.println("Книга не найдена или занята");
             return false;
         } else {
-b
+            book.isBusy();
+            activeUser.getUserBooks().add(book);
+            System.out.println("Книга успешно взята читателем");
+            return true;
         }
-
-        return true;
     }
 
     @Override
     public boolean returnBook(int id) {
-        return false;
+        /*
+        получить книгу из репозитория и присвоить её в переменную
+        проверить, что она найдена и занята,
+        если нет - закончить работу метода,
+        если да  - пометить книгу как свободную,
+                   удалить книгу из списка книг текущего пользователя
+         */
+        Book book = repositoryBook.getBookById(id);
+        if (book == null || !book.isBusy()) {
+            System.out.println("Книга не найдена");
+            return false;
+        } else {
+            book.setBusy(false);
+            activeUser.getUserBooks().remove(book);
+            System.out.println("Книга успешно возвращена в библиотеку");
+            return true;
+        }
     }
 
     @Override
     public MyList<Book> getAllUsersBook(User user) {
-        return null;
+        /*
+        проверяем переданного в метод юзера:
+            не равен ли null
+            есть ли он в нашей базе данных (по имейлу)
+        получаем список книг этого юзера из репозитория
+         */
+        if (user == null || repositoryUser.isEmailExist(user.getEmail())) {
+            System.out.println("Пользователь не найден");
+            return new MyArrayList<>();
+        } else {
+            MyList<Book> usersBooks = repositoryUser.getAllUsersBook(user);
+            return usersBooks;
+        }
     }
+
+    //-----------------------------------------Methods of Users------------------------------
 
     @Override
     public User registerUser(String email, String password) {
+        if (repositoryUser.isEmailExist(email)) {
+            System.out.println("Пользователь с таким email уже существует");
+            return null;
+        } else {
+
+        }
         return null;
     }
+
+
+
 
     @Override
     public boolean loginUser(String email, String password) {
